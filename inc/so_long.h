a2194186022d285268590f05224c777a1182f9e3
Vincent Mercier <vincent.mrc@hotmail.com>
2022-04-26 15:02:26 +0200

    Getting started

---
+++ inc/so_long.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vimercie <vimercie@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/26 14:44:18 by vimercie          #+#    #+#             */
+/*   Updated: 2022/04/26 14:47:54 by vimercie         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+
+# define PUSH_SWAP_H
+
+# include <math.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include "../Libft/libft.h"
+
+
+
+#endif
